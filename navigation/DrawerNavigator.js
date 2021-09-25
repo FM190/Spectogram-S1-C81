243dc5809ea6136520fcadbb2551e2c3aacb88ea
FM190 <86798311+FM190@users.noreply.github.com>
2021-09-25 14:49:45 -0400

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import TabNavigator from './TabNavigation';
+import Profile from '../screens/Profile';
+
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator =()=>{
+  return(
+    <Drawer.Navigator>
+    <Drawer.Screen name="Home" component={TabNavigator}/>
+    <Drawer.Screen name="Profile" component={Profile}/>
+    </Drawer.Navigator>
+  )
+}
+
+export default DrawerNavigator;
